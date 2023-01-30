a1509c3d9f71059ecb7eb73a80c17d6f29b4d6b2
Timur <cpdbox@mail.ru>
2023-01-30 20:42:30 +0300

    06

---
+++ 06/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит случайное 
+//трёхзначное число и удаляет вторую цифру 
+//этого числа.
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+Console.Clear();
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"Num = {num}");
+int result = (num / 100) * 10 + num % 10;
+Console.WriteLine($"Результат = {result}");
+
+
+Console.Clear();
+int num = new Random().Next(100, 1000);
+Console.WriteLine($"{num} -> {result}");
+Console.WriteLine($"{num} -> {num/100}{num%10}");
