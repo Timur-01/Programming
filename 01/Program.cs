e4b70ac80a738a7007503dee1b344a0a6964761c
Timur <cpdbox@mail.ru>
2023-01-30 15:56:10 +0300

    Task

---
+++ 01/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.Clear();
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if(number1 * number1 == number2){
+    Console.Write("Да");    
+}
+else{
+    Console.Write("Нет"); 
+}
---
+++ 04/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N.
+Console.Clear();
+Console.WriteLine("Первый метод");
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100 || num > 999){
+    Console.WriteLine("Введено не првильное число");
+    return;
+}
+int otvet = num % 10;
+Console.WriteLine($"3 цифра: {otvet}");
+Console.WriteLine("----------------");
+Console.WriteLine("Второй метод");
+string number2 = Console.ReadLine();
+string otvet2 = Convert.ToString(number2[number2.Length - 1]);
+Console.WriteLine($"3 цифра: {otvet2}");
