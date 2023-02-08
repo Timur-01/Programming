34fd3b81e5de5204f0951fdfafad5983e62a56a5
Timur <cpdbox@mail.ru>
2023-02-08 23:16:32 +0300

    06

--- 06/Program.cs
+++ 06/Program.cs
@@ -5,14 +5,14 @@
 //782 -> 72
 //918 -> 98
 
-Console.Clear();
-int num = new Random().Next(100, 1000);
-Console.WriteLine($"Num = {num}");
-int result = (num / 100) * 10 + num % 10;
-Console.WriteLine($"Результат = {result}");
+// Console.Clear();
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine($"Num = {num}");
+// int result = (num / 100) * 10 + num % 10;
+// Console.WriteLine($"Результат = {result}");
 
 
 Console.Clear();
 int num = new Random().Next(100, 1000);
-Console.WriteLine($"{num} -> {result}");
+// Console.WriteLine($"{num} -> {result}");
 Console.WriteLine($"{num} -> {num/100}{num%10}");
