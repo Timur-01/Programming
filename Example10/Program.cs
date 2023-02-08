7bd4546f97aca2b6eeb7e7916562e5c5efc7cc5f
Timur <cpdbox@mail.ru>
2023-02-08 22:10:16 +0300

    Example10

--- Example10/Program.cs
+++ Example10/Program.cs
@@ -8,17 +8,4 @@ Console.Clear();
 Console.Write("Введите трехзначное число: ");
 string number = Console.ReadLine();
 string otvet = Convert.ToString(number[number.Length - 2]);
-Console.WriteLine($"2 цифра: {otvet}");
-
-// Console.Clear();
-// Console.WriteLine("Первый метод");
-// Console.Write("Введите трехзначное число: ");
-// int num = int.Parse(Console.ReadLine());
-// if (num < 100 || num > 999){
-//     Console.WriteLine("Введено не првильное число");
-//     return;
-// }
-// int num2 = num / 10;
-// int otvet = num2 % 10;
-// Console.WriteLine($"3 цифра: {otvet}");
-// Console.WriteLine("----------------");
+Console.WriteLine($"Вторая цифра: {otvet}");
