02929b0a688b781c4b2f7f354a5e47a0df46fd73
Timur <cpdbox@mail.ru>
2023-02-18 15:00:57 +0300

    Example336

---
+++ Example36/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 36: Задайте одномерный массив,
+// заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+int[] elArray = GetArray(4, 1, 999);
+Console.WriteLine(String.Join(" ", elArray));
+int oddSum = 0;
+
+    for (int i = 0; i < elArray.Length; i++)
+{
+    int el = elArray[i];
+    if (el % 2 != 0)
+       oddSum += el;
+}
+   
+Console.WriteLine($"Сумма нечётных чисел {oddSum}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+
+
+//     for (int i = 0; i < elArray.Length; i++)
+// {
+//     int el = elArray[i];
+//     if (el % 2 != 1)
+//        oddSum += el;
+// }
+   
+// Console.WriteLine($"Сумма нечётных чисел {oddSum}");
+
+  
