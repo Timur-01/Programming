2992ea0dfb0d6209902565f88fdf644cb4246d2f
Timur <cpdbox@mail.ru>
2023-02-19 12:31:31 +0300

    Example36

--- Example36/Program.cs
+++ Example36/Program.cs
@@ -3,8 +3,29 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+// Console.Clear();
+// // Console.Write("Введите элементы массива через пробел: ");
+// // string elements = Console.ReadLine();
+// int[] elArray = GetArray(4, 1, 999);
+// int[] elArray = GetArrayFromString(elements);
+// Console.WriteLine(String.Join(" ", GetArray(elArray)));
+
+// Console.WriteLine(String.Join(" ", ResultArray(baseArray)));
+
+// int[] GetArrayFromString(string stringArray)
+// {
+//     string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+//     int[] res = new int[nums.Length];
+
+//     for (int i = 0; i < nums.Length; i++)
+//     {
+//         res[i] = int.Parse(nums[i]);
+//     }
+//     return res;
+// }
+
 Console.Clear();
-int[] elArray = GetArray(4, 1, 999);
+int[] elArray = GetArray(4, 1, 1000);
 Console.WriteLine(String.Join(" ", elArray));
 int oddSum = 0;
 
@@ -14,7 +35,7 @@ int oddSum = 0;
     if (el % 2 != 0)
        oddSum += el;
 }
-   
+
 Console.WriteLine($"Сумма нечётных чисел {oddSum}");
 
 int[] GetArray(int size, int minValue, int maxValue)
@@ -29,14 +50,3 @@ int[] GetArray(int size, int minValue, int maxValue)
 }
 
 
-
-//     for (int i = 0; i < elArray.Length; i++)
-// {
-//     int el = elArray[i];
-//     if (el % 2 != 1)
-//        oddSum += el;
-// }
-   
-// Console.WriteLine($"Сумма нечётных чисел {oddSum}");
-
-  
