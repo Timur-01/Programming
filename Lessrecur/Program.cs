26d721da292afa34a2b66a2d86d4afbda8bf3e17
Timur <cpdbox@mail.ru>
2023-03-25 17:31:32 +0300

    Lessrecur

---
+++ Lessrecur/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Задайте значение N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int summ = 0;
+
+int SumRec(int M, int N, int summ = 0)
+{
+if (M > N)
+{
+M = N;
+N = summ;
+}
+summ = summ + N;
+
+return N <= M ? N : SumRec(M, N - 1) + summ;
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write($"Сумма элементов= {SumRec(M, N, summ)} ");
+Console.ForegroundColor = ConsoleColor.White;
