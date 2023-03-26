f3c440f3de49b7c17c7df19b4d211020e1e203f2
Timur <cpdbox@mail.ru>
2023-03-26 13:32:37 +0300

    Example66

--- Example66/Program.cs
+++ Example66/Program.cs
@@ -14,22 +14,6 @@ int n = int.Parse(ReadLine());
 Write("Введите end: ");
 int m = int.Parse(ReadLine());
 
-// WriteLine($"{m}->{SumNumbers(m)}");
-// int sum = 0;
-// while(m > 0){
-//     sum += m % 10; // <=> sum = sum + m % 10
-//     m /= 10; // <=> m = m / 10
-// }
-// WriteLine($"{sum}");
-
-
-// int SumNumbers(int number)
-// {
-//     if (number == 0)
-//         return 0;
-//     return (number % 10 + SumNumbers(number / 10));
-// }
-
 WriteLine($"{SumNumber(n, m)}");
 
 int SumNumber(int n, int m)
